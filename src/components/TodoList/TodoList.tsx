d4729312f29ee8a3d3cd6d6027227b82d878675e
r1ck12 <0xr1ck12@gmail.com>
2023-11-16 18:26:32 -0300

    refactor: create TodoListItem

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,10 +1,11 @@
-import { Box, Card, Checkbox, Flex, Select, Strong, TextField, Text } from '@radix-ui/themes';
+import { Box, TextField } from '@radix-ui/themes';
 import { useEffect, useState } from 'react';
 import { baseRequest } from '../../utils/services/requests';
 import { Category, Todo } from '../../types';
+import TodoListItem from './TodoListItem';
 
 type TodoListProps = {
-  categories: { color: string; id: number; name: string }[];
+  categories: Category[];
 };
 
 const TodoList = ({ categories }: TodoListProps) => {
@@ -23,41 +24,6 @@ const TodoList = ({ categories }: TodoListProps) => {
   const onCreateTodoKeyDown = (e: any) => {
     if (e.key === 'Enter') {
       addTodo();
-    }
-  };
-
-  const toggleTodo = async (id: number) => {
-    const todo = todos.find((todo: Todo) => todo.id === id);
-    if (todo) {
-      const updatedTodo = await baseRequest(`todos/${id}`, 'PUT', {
-        ...todo,
-        done: !todo.done,
-      });
-      const updatedTodos = todos.map((todo: Todo) => {
-        if (todo.id === updatedTodo.id) {
-          return updatedTodo;
-        }
-        return todo;
-      });
-      setTodos(updatedTodos);
-    }
-  };
-
-  const onTodoCategoryChange = async (value: string, todoId: number) => {
-    const todo = todos.find((todo: Todo) => todo.id === todoId);
-
-    if (todo) {
-      const updatedTodo = await baseRequest(`todos/${todoId}`, 'PUT', {
-        ...todo,
-        categoryId: parseInt(value),
-      });
-      const updatedTodos = todos.map((todo: Todo) => {
-        if (todo.id === updatedTodo.id) {
-          return updatedTodo;
-        }
-        return todo;
-      });
-      setTodos(updatedTodos);
     }
   };
 
@@ -84,53 +50,7 @@ const TodoList = ({ categories }: TodoListProps) => {
       </TextField.Root>
 
       {todos.map((todo: Todo) => {
-        return (
-          <Card
-            my="2"
-            key={todo.id}
-            style={{
-              backgroundColor: categories.find((category: Category) => category.id === todo.categoryId)?.color,
-            }}
-          >
-            <Flex gap="3" align="center">
-              <Checkbox
-                size="3"
-                checked={todo.done}
-                onCheckedChange={() => {
-                  toggleTodo(todo.id);
-                }}
-              />
-              <Box>
-                <Text
-                  as="span"
-                  size="3"
-                  style={{
-                    color: 'black',
-                  }}
-                >
-                  <Strong> {todo.text}</Strong>
-                </Text>
-              </Box>
-              <Select.Root
-                value={todo.categoryId?.toString()}
-                onValueChange={(value) => onTodoCategoryChange(value, todo.id)}
-              >
-                <Select.Trigger />
-                <Select.Content>
-                  <Select.Group>
-                    {categories.map((category: Category) => {
-                      return (
-                        <Select.Item key={category.id} value={category.id.toString()}>
-                          {category.name}
-                        </Select.Item>
-                      );
-                    })}
-                  </Select.Group>
-                </Select.Content>
-              </Select.Root>
-            </Flex>
-          </Card>
-        );
+        return <TodoListItem todo={todo} todos={todos} setTodos={setTodos} categories={categories} />;
       })}
     </Box>
   );
---
+++ src/components/TodoList/TodoListItem.tsx
@@ -0,0 +1,97 @@
+import { Card, Flex, Checkbox, Box, Strong, Select, Text } from '@radix-ui/themes';
+import { Category, Todo } from '../../types';
+import { baseRequest } from '../../utils/services/requests';
+import { Dispatch, SetStateAction } from 'react';
+
+type TodoListItemProps = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: Dispatch<SetStateAction<Todo[]>>;
+  categories: Category[];
+};
+
+const TodoListItem = ({ todo, todos, setTodos, categories }: TodoListItemProps) => {
+  const toggleTodo = async (todo: Todo) => {
+    if (todo) {
+      const updatedTodo = await baseRequest(`todos/${todo.id}`, 'PUT', {
+        ...todo,
+        done: !todo.done,
+      });
+      const updatedTodos = todos.map((todo: Todo) => {
+        if (todo.id === updatedTodo.id) {
+          return updatedTodo;
+        }
+        return todo;
+      });
+      setTodos(updatedTodos);
+    }
+  };
+
+  const onTodoCategoryChange = async (value: string, todoId: number) => {
+    const todo = todos.find((todo: Todo) => todo.id === todoId);
+
+    if (todo) {
+      const updatedTodo = await baseRequest(`todos/${todoId}`, 'PUT', {
+        ...todo,
+        categoryId: parseInt(value),
+      });
+      const updatedTodos = todos.map((todo: Todo) => {
+        if (todo.id === updatedTodo.id) {
+          return updatedTodo;
+        }
+        return todo;
+      });
+      setTodos(updatedTodos);
+    }
+  };
+
+  return (
+    <Card
+      my="2"
+      key={todo.id}
+      style={{
+        backgroundColor: categories.find((category: Category) => category.id === todo.categoryId)?.color,
+      }}
+    >
+      <Flex gap="3" align="center">
+        <Checkbox
+          size="3"
+          checked={todo.done}
+          onCheckedChange={() => {
+            toggleTodo(todo);
+          }}
+        />
+        <Box>
+          <Text
+            as="span"
+            size="3"
+            style={{
+              color: 'black',
+            }}
+          >
+            <Strong> {todo.text}</Strong>
+          </Text>
+        </Box>
+        <Select.Root
+          value={todo.categoryId?.toString()}
+          onValueChange={(value) => onTodoCategoryChange(value, todo.id)}
+        >
+          <Select.Trigger />
+          <Select.Content>
+            <Select.Group>
+              {categories.map((category: Category) => {
+                return (
+                  <Select.Item key={category.id} value={category.id.toString()}>
+                    {category.name}
+                  </Select.Item>
+                );
+              })}
+            </Select.Group>
+          </Select.Content>
+        </Select.Root>
+      </Flex>
+    </Card>
+  );
+};
+
+export default TodoListItem;
